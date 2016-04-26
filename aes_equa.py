fcccf796493e347e1ac2109b4e9f1e476349434a
MyShell <myshell.dubois@gmail.com>
2016-04-26 19:40:28 +0200

    Full encryption files

--- aes_equa.py
+++ aes_equa.py
@@ -1,6 +1,7 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import shutil
 
 from libmain import *
 from libsubbytes import *
@@ -8,15 +9,15 @@ from libshiftrows import *
 from libmixcolumns import *
 from libkeyexpansion import *
 from libkeyexpansion import *
-from aes_equa_enc import *
-from aes_equa_dec import *
+from libequaenc import *
+from libequadec import *
 
 
-def testAESdirectory(val):
+def testAESdirectory():
 	d = os.path.dirname(directory)
 	if os.path.exists(d):
-		os.rmdir(directory)
-
+		printColor('## Deleting directory %s' % (d), RED)
+		shutil.rmtree(directory)
 
 
 def encryptionProcess(step=True):
@@ -37,10 +38,51 @@ def decryptionProcess(step=True):
 		controlDecFullFiles()
 
 
+def getNumRoundEnc(line):
+	line = line[3:len(line)]
+	if line.startswith('add'):
+		n = line[11:len(line)]
+	if line.startswith('Round'):
+		n = line[5:len(line)]
+	if line.startswith('end'):
+		n = 100
+	return int(n)
 
+
+def treatLines(allLines):
+	result = []
+	emptyLine = '0 00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000'
+	for line in allLines:
+		line = line.rstrip('\n')
+		if line.startswith('##'):
+			numRound = getNumRoundEnc(line)
+			result.append(line + '\n')
+		else:
+			line = line[:1] + ' ' + line[2:]
+			line = (numRound * (emptyLine + ' ')) + line + ((10 - numRound) * (' ' + emptyLine))
+			result.append(line + '\n')
+	return result
+
+
+def createFullEncryptionFiles():
+	testAESdirectory()
+	generateEncFullFiles()
+	for i in xrange(blockSize):
+		fileName = fileNameEnc+'%s.txt' % intToThreeChar(i)
+		allLines = readFile(fileName)
+		result = treatLines(allLines)
+		f = open(directory+fileName, 'w')
+		for line in result:
+			f.write(line)
+		closeFile(f)
+		print 'bit number', i, ':', len(allLines), 'lines readed,', len(result), 'lines treated'
+	printColor('## Files generated', RED)
 
 
 
 
 if __name__ == "__main__":
 	#testKeyExpansion()
+	#encryptionProcess()
+	#decryptionProcess()
+	createFullEncryptionFiles()
